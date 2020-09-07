4dd419599d2463c32255b70d08344c5a3e3a1b65
Joakim Andersson <00joaand@mk.se>
2020-09-07 18:30:19 +0200

    Add project files.

---
+++ Inlämningsuppgift 1/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+namespace Inlämningsuppgift_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // deklarera variabler & annat för att avändaren ska kunna ange operatorer och värden
+            decimal a, b, c, svar;
+            string op1, op2;
+
+            // läs in vad användaren anger
+            Console.WriteLine("Ange operatör nummer 1:");
+            op1 = Console.ReadLine();
+            Console.WriteLine("Ange operatör nummer 2:");
+            op2 = Console.ReadLine();
+            Console.WriteLine("Ange första term:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ange andra term:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ange tredje term:");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            // beräkna & skriv ut resultat
+            if (op1 == "+" & op2 == "+")
+            {
+                svar = a + b + c;
+                Console.WriteLine(a + "+" + b + "+" +  c + "=" + svar);
+            }
+
+            else if (op1 == "+" & op2 == "-")
+            {
+                svar = a + b - c;
+                Console.WriteLine(a + "+" + b + "-" + c + "=" + svar);
+            }
+
+            else if (op1 == "+" & op2 == "*")
+            {
+                svar = a + b * c;
+                Console.WriteLine(a + "+" + b + "*" + c + "=" + svar);
+            }
+
+            else if (op1 == "+" & op2 == "/")
+            {
+                svar = a + b / c;
+                Console.WriteLine(a + "+" + b + "/" + c + "=" + svar);
+            }
+
+            else if (op1 == "-" & op2 == "+")
+            {
+                svar = a - b + c;
+                Console.WriteLine(a + "-" + b + "+" + c + "=" + svar);
+            }
+
+            else if (op1 == "-" & op2 == "-")
+            {
+                svar = a - b - c;
+                Console.WriteLine(a + "-" + b + "-" + c + "=" + svar);
+            }
+
+            else if (op1 == "-" & op2 == "*")
+            {
+                svar = a - b * c;
+                Console.WriteLine(a + "-" + b + "*" + c + "=" + svar);
+            }
+
+            else if (op1 == "-" & op2 == "/")
+            {
+                svar = a - b / c;
+                Console.WriteLine(a + "-" + b + "/" + c + "=" + svar);
+            }
+
+            else if (op1 == "*" & op2 == "+")
+            {
+                svar = a * b + c;
+                Console.WriteLine(a + "*" + b + "+" + c + "=" + svar);
+            }
+
+            else if (op1 == "*" & op2 == "-")
+            {
+                svar = a * b - c;
+                Console.WriteLine(a + "*" + b + "-" + c + "=" + svar);
+            }
+
+            else if (op1 == "*" & op2 == "*")
+            {
+                svar = a * b * c;
+                Console.WriteLine(a + "*" + b + "*" + c + "=" + svar);
+            }
+
+            else if (op1 == "*" & op2 == "/")
+            {
+                svar = a * b / c;
+                Console.WriteLine(a + "*" + b + "/" + c + "=" + svar);
+            }
+
+            else if (op1 == "/" & op2 == "+")
+            {
+                svar = a / b + c;
+                Console.WriteLine(a + "/" + b + "+" + c + "=" + svar);
+            }
+
+            else if (op1 == "/" & op2 == "-")
+            {
+                svar = a / b - c;
+                Console.WriteLine(a + "/" + b + "-" + c + "=" + svar);
+            }
+
+            else if (op1 == "/" & op2 == "*")
+            {
+                svar = a / b * c;
+                Console.WriteLine(a + "/" + b + "*" + c + "=" + svar);
+            }
+
+            else if (op1 == "/" & op2 == "/")
+            {
+                svar = a / b / c;
+                Console.WriteLine(a + "/" + b + "/" + c + "=" + svar);
+            }
+
+            else
+            {
+
+            }
+
+        }
+    }
+}
